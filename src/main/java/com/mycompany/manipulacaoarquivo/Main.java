3513f28a7d1ca3610fb382c4824cf5bcfc0cbdc7
mariacarolinass <m.carolina.soares1@gmail.com>
2022-03-22 14:54:47 -0300

    Implementando a funcao pesquisar coordenadas

--- src/main/java/com/mycompany/manipulacaoarquivo/Main.java
+++ src/main/java/com/mycompany/manipulacaoarquivo/Main.java
@@ -12,20 +12,36 @@ public class Main {
         
         ArrayList <String[]> attributes = File.reader(path);
         
+        // Retornando as coordenadas
+        getCoordinate("0294-0181", "101", attributes);
+        
     }
     
     // Pesquisando por coordenadas na lista
     public static void getCoordinate(String lineName, String pointNumber, 
             ArrayList <String[]> attributes) {
         
-        /* while (attributes) {
-            
-            System.out.println();
-            
-        }
+        // Percorrendo pela lista de atrihutos
+        for (String[] list: attributes) {
         
-        System.out.println("Não encontrado!");
-        */
+            // Verificando se os atributos lineName e pointNumber correspondem
+            if (list[0].equals(lineName) && list[1].equals(pointNumber)) {
+                
+                // Imprimindo os valores das coordenadas
+                System.out.println("Latitude: " + list[2]);
+                System.out.println("Longitude: " + list[3]);
+                System.out.println("X: " + list[4]);
+                System.out.println("Y: " + list[5]);
+                System.out.println("Z: " + list[6]);
+                
+                return;
+             }
+        
+         }
+        
+        // Se a coordenada não corresponder
+        System.out.println("Coordenadas não encontradas!");
+        
     }
     
 }
