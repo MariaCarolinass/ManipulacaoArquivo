ff3dd338d0e75cdff92340cdb0eeb0eea91d2b4b
mariacarolinass <m.carolina.soares1@gmail.com>
2022-03-23 15:43:33 -0300

    add atributos p/ a classe

---
+++ src/main/java/file/FileClass.java
@@ -0,0 +1,49 @@
+package file;
+
+import com.mycompany.CoordinateClass.CoordinateClass;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class FileClass {
+    
+    // Leitura do arquivo armazenado na classe
+    public static ArrayList<CoordinateClass> reader(String path) throws IOException {
+        
+        // Lista para armazenar os atributos na classe
+        ArrayList <CoordinateClass> attributes2 = new ArrayList();
+        
+        // Carregamento dos dados salvo na memória Buffer
+        try (BufferedReader buffRead = new BufferedReader(new FileReader(path))) {
+            String line = "";
+            
+            // Carrega as linhas do arquivo txt
+            while ((line = buffRead.readLine()) != null) {
+                
+                // Ignorar todas as linhas que começam com H
+                if (!line.startsWith("H")) {
+                    
+                    // Atributos do arquivo organizados e trasformados em string
+                    String lineName = line.substring(1, 13).trim(); 
+                    Integer pointNumber = Integer.parseInt(line.substring(19, 25).trim()); 
+                    String latitude = line.substring(25, 35).trim();
+                    String longitude = line.substring(35, 46).trim();
+                    Double x = Double.parseDouble(line.substring(46, 55).trim());
+                    Double y = Double.parseDouble(line.substring(55, 64).trim());
+                    Double z = Double.parseDouble(line.substring(64, 70).trim());
+                    
+                    // Atributos passados para a lista
+                    attributes2.add(new CoordinateClass(lineName, pointNumber, 
+                        latitude, longitude, x, y, z));
+                }
+               
+            }
+            
+        }
+        
+        return attributes2;
+    }
+    
+} 
+
