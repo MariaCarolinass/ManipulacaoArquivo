741c9b152cb5d5544b3b00f1266555125ab12494
mariacarolinass <m.carolina.soares1@gmail.com>
2022-03-23 13:06:11 -0300

    alterei nome da variavel

--- src/main/java/com/mycompany/CoordinateList/Coordinate.java
+++ src/main/java/com/mycompany/CoordinateList/Coordinate.java
@@ -9,17 +9,17 @@ public class Coordinate {
             ArrayList <String[]> attributes) {
         
         // Percorrendo pela lista de atrihutos
-        for (String[] list: attributes) {
+        for (String[] coord: attributes) {
         
             // Verificando se os atributos lineName e pointNumber correspondem
-            if (list[0].equals(lineName) && list[1].equals(pointNumber)) {
+            if (coord[0].equals(lineName) && coord[1].equals(pointNumber)) {
                 
                 // Imprimindo os valores das coordenadas
-                System.out.println("Latitude: " + list[2]);
-                System.out.println("Longitude: " + list[3]);
-                System.out.println("X: " + list[4]);
-                System.out.println("Y: " + list[5]);
-                System.out.println("Z: " + list[6]);
+                System.out.println("Latitude: " + coord[2]);
+                System.out.println("Longitude: " + coord[3]);
+                System.out.println("X: " + coord[4]);
+                System.out.println("Y: " + coord[5]);
+                System.out.println("Z: " + coord[6]);
                 
                 return;
              }
