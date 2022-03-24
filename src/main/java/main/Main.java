2c58eac1680a1264d44e422b555622604aec1d47
mariacarolinass <m.carolina.soares1@gmail.com>
2022-03-24 16:21:19 -0300

    chamei metodo de pesquisar no mapa

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,26 +1,36 @@
 package main;
 
+/* import coordinatelist.CoordinateList;
+import static coordinatelist.CoordinateList.getCoordinateList;
+import file.FileList;
 import coordinateclass.CoordinateClass;
 import static coordinateclass.CoordinateClass.getCoordinateClass;
-import file.FileClass;
-import java.io.IOException;
+import file.FileClass; */
+import coordinatemap.CoordinateMap;
+import file.FileMap;
+import static coordinatemap.CoordinateMap.getCoordinateMap;
+import java.util.Map;
 import java.util.ArrayList;
+import java.io.IOException;
 
 public class Main {
 
-    // Armazenando o documento txt em uma String e adicionando na lista
     public static void main(String[] args) throws IOException {
         
+        // Armazenando o documento txt em uma String
         String path = "/Z/soarescarol/compartilhado/0294_POT_302_BOA_VISTA_2D.UKOOA.txt";
         
-        /* Retornando as coordenadas da lista
-        ArrayList <String[]> attributes = File.reader(path);
+        /*Retornando as coordenadas usando lista
+        ArrayList <String[]> attributes = FileList.reader(path);
         getCoordinateList("0294-0181", "101", attributes); */
         
-        // Retornando as coordenadas da lista por meio de uma classe
+        /* Retornando as coordenadas usando classe
         ArrayList <CoordinateClass> attributes2 = FileClass.reader(path);
-        getCoordinateClass("0294-0181", 102, attributes2);
+        getCoordinateClass("0294-0181", 102, attributes2); */
        
+        // Retornando as coordenadas usando Map
+        Map <String, ArrayList<CoordinateMap>> attributes3 = FileMap.reader(path);
+        getCoordinateMap("0294-0181", 103, attributes3);
     }
 
 }
